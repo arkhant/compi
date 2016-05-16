177f7e58cfc29f2999bae36469110dd50fe6c450
arkhant <arkhant@hotmail.com>
2016-05-16 03:06:53 -0500

    1.- inicio del proyecto

---
+++ src/compilador/Compilador.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+import java.io.FileReader;
+
+/**
+ *
+ * @author nemea
+ */
+public class Compilador {
+//
+    Compilador() {
+            String path;
+            parser ThisParser;
+            Yylex ThisScanner;
+            path="entrada.cpp";
+        try
+            { 
+                ThisScanner= new Yylex (new FileReader (path));
+
+                ThisParser=new parser (ThisScanner);
+                ThisParser.parse ();
+
+                Interfaz.setTexto2("FIN DEL ESCANEO");
+            }catch(Exception e)
+                {Interfaz.setTexto2("SE TRUNCO EL PROCESO "+e.toString());}
+        }
+    //
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    new Interfaz().setVisible(true);        
+// TODO code application logic here
+    }
+    
+}
